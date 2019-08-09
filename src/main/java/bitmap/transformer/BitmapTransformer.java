cb95aaf6e7053549a4b02fb212c859c3b5ae1d51
Manish KC <kcmanish89@gmail.com>
2019-08-09 12:11:26 -0700

    paired programmed to complete the lab

---
+++ src/main/java/bitmap/transformer/BitmapTransformer.java
@@ -0,0 +1,104 @@
+package bitmap.transformer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class BitmapTransformer {
+
+    public  String changeToGrey(String PATH, String outputPath) throws IOException {
+        int width, height;  //Variable declarations
+        File bmpFile = new File (PATH);
+        BufferedImage bmpImage = ImageIO.read(bmpFile);
+        width = bmpImage.getWidth();
+        height = bmpImage.getHeight();
+
+        for (int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                Color mycolor = new Color(bmpImage.getRGB(x, y));
+                if (mycolor.getBlue() == 0 && mycolor.getRed() == 0 && mycolor.getGreen() == 0) {
+                    Color grey = new Color(100,100,100);
+                    int rgb = grey.getRGB();
+                    bmpImage.setRGB(x, y, rgb );
+
+                }
+
+            }
+        }
+
+        File outfile = new File(outputPath);
+        ImageIO.write(bmpImage, "bmp", outfile);
+
+
+        return "Succesfully created in directory: " + outputPath;
+    }
+
+
+    public String flipImage(String PATH, String outputPath) throws IOException {
+        int width, height;  //Variable declarations
+        File bmpFile = new File (PATH);
+        BufferedImage bmpImage = ImageIO.read(bmpFile);
+        width = bmpImage.getWidth();
+        height = bmpImage.getHeight();
+
+
+        int[][] pixels = new int[width][height];
+        for( int i = 0; i < width; i++ )
+            for( int j = 0; j < height; j++ )
+                pixels[i][j] = bmpImage.getRGB( i, j );
+
+        for(int i = 0; i < pixels.length / 2; i++) {
+            int[] temp = pixels[i];
+            pixels[i] = pixels[pixels.length - i - 1];
+            pixels[pixels.length - i - 1] = temp;
+        }
+        for(int j = 0; j < pixels.length; j++){
+            for(int i = 0; i < pixels[j].length / 2; i++) {
+                int temp = pixels[j][i];
+                pixels[j][i] = pixels[j][pixels[j].length - i - 1];
+                pixels[j][pixels[j].length - i - 1] = temp;
+            }
+        }
+
+
+        for(int j = 0; j < pixels.length; j++){
+            for(int i = 0; i < pixels[j].length; i++) {
+                int rgb = pixels[j][i];
+                bmpImage.setRGB(j, i, rgb );
+
+            }
+        }
+
+        File outfile = new File(outputPath);
+        ImageIO.write(bmpImage, "bmp", outfile);
+
+
+        return "Succesfully created in directory: " + outputPath;
+
+
+    }
+
+
+
+    public String resize(String PATH, String outputPath) throws IOException {
+        System.out.println("in resize");
+        File bmpFile = new File (PATH);
+        BufferedImage bmpImage = ImageIO.read(bmpFile);
+
+        Image tmp = bmpImage.getScaledInstance(bmpImage.getWidth(), bmpImage.getHeight(), Image.SCALE_SMOOTH);
+
+        BufferedImage resized = new BufferedImage(bmpImage.getWidth()/2 + 50, bmpImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resized.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        File outfile = new File(outputPath);
+        ImageIO.write(resized, "png", outfile);
+
+        return "success";
+
+    }
+}
